9ffa1f5cf1f472a262e5e74d3d57bfc625d69fdf
Luo <Alberto.hugo05@gmail.com>
2016-08-09 22:25:17 +0200

    Updated result view

--- app/src/main/java/io/deuxsept/dndice/Main/HomeFragment.kt
+++ app/src/main/java/io/deuxsept/dndice/Main/HomeFragment.kt
@@ -6,20 +6,27 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import butterknife.Bind
-import butterknife.ButterKnife
-import butterknife.OnClick
 import io.deuxsept.dndice.R
 
 class HomeFragment : Fragment() {
 
-    lateinit var mResultView: View
-    lateinit var mResultVar: TextView
+    /**
+     * Home Views
+     */
     lateinit var mRollButton: View
+    lateinit var mDisplay: TextView
+
+    /**
+     * Result Views
+     */
+    lateinit var mResultView: View
+    lateinit var mFormula: TextView
+    lateinit var mResult: TextView
+    lateinit var mDetail: TextView
+    lateinit var mFavoriteFab: FloatingActionButton
     lateinit var mCloseResFab: FloatingActionButton
+    lateinit var mReplayFab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,23 +35,15 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.fragment_home, container, false)
 
-        mResultView = view.findViewById(R.id.result_view)
-        mResultVar = view.findViewById(R.id.result_var) as TextView
+        mDisplay = view.findViewById(R.id.display) as TextView
         mRollButton = view.findViewById(R.id.roll_button)
         mRollButton.setOnClickListener {
             openResultView()
         }
-        mCloseResFab = view.findViewById(R.id.close_result_fab) as FloatingActionButton
-        mCloseResFab.setOnClickListener {
-            closeResultView()
-        }
+
+        initResultViewVars(view)
 
         return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        ButterKnife.unbind(this)
     }
 
     companion object {
@@ -62,7 +61,35 @@ class HomeFragment : Fragment() {
         mResultView.visibility = View.VISIBLE
     }
 
+    fun initResultViewVars(view: View) {
+        mResultView = view.findViewById(R.id.result_view)
+        mFormula = view.findViewById(R.id.formula_var) as TextView
+        mResult = view.findViewById(R.id.result_var) as TextView
+        mDetail = view.findViewById(R.id.detail_var) as TextView
+
+        mFavoriteFab = view.findViewById(R.id.replay_result_fab) as FloatingActionButton
+        mFavoriteFab.setOnClickListener {
+            favoriteRoll()
+        }
+        mCloseResFab = view.findViewById(R.id.close_result_fab) as FloatingActionButton
+        mCloseResFab.setOnClickListener {
+            closeResultView()
+        }
+        mReplayFab = view.findViewById(R.id.fav_result_fab) as FloatingActionButton
+        mReplayFab.setOnClickListener {
+            reRoll()
+        }
+    }
+
+    fun favoriteRoll() {
+
+    }
+
     fun closeResultView() {
         mResultView.visibility = View.GONE
+    }
+
+    fun reRoll() {
+
     }
 }
