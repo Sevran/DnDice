79a7b9c94bbe1ee5f22d219f35d2328ed303a451
Florian Eula <mr.pikzen@gmail.com>
2016-08-24 01:25:33 +0200

    Android Studio does things and noone understands

---
+++ app/src/main/java/io/deuxsept/dndice/SettingsView/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package io.deuxsept.dndice.SettingsView
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
---
+++ app/src/main/java/io/deuxsept/dndice/SettingsView/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package io.deuxsept.dndice.SettingsView
+
+/**
+ * Created by Flo on 24/08/2016.
+ */
+class SettingsFragment {
+}
